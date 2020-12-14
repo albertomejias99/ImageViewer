1f033ef3801cf4bf66c13be7f79b131bfc864bc1
albertomejias99 <72130145+albertomejias99@users.noreply.github.com>
2020-12-14 20:09:51 +0000

    ImageViewer_MVC

---
+++ ImageViewer/src/imageviewer/apps/swing/Main.java
@@ -0,0 +1,72 @@
+package imageviewer.apps.swing;
+
+import imageviewer.control.Command;
+import imageviewer.control.NextImageCommand;
+import imageviewer.control.PrevImageCommand;
+import imageviewer.model.Image;
+import imageviewer.view.ImageDisplay;
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JMenuBar;
+import javax.swing.JPanel;
+
+public class Main extends JFrame{
+    
+    private ImageDisplay imageDisplay;
+    private List<Image> images;
+    private Map<String,Command> commands = new HashMap<>();
+    
+    public static void main(String[] args) {
+        new Main().execute();
+    }
+
+    public Main() {
+        this.setTitle("ImageViewer");
+        this.setSize(800,600);
+        this.setLocationRelativeTo(null);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.getContentPane().add(imagePanel());
+        this.add(toolbar(),BorderLayout.SOUTH);
+    }
+
+    private void execute() {
+        this.images = new FileImageLoader(new File("fotos")).load();
+        this.imageDisplay.display(images.get(0));
+        this.commands.put("<",new PrevImageCommand(images,imageDisplay));
+        this.commands.put(">",new NextImageCommand(images,imageDisplay));
+        this.setVisible(true);
+    }
+
+    private JPanel imagePanel() {
+        BlockPanel blockPanel = new BlockPanel();
+        this.imageDisplay = blockPanel;
+        return blockPanel;
+    } 
+
+    private JMenuBar toolbar() {
+        JMenuBar toolbar = new JMenuBar();
+        toolbar.add(button("<"));
+        toolbar.add(button(">"));
+        return toolbar;
+    }
+
+    private JButton button(String name) {
+        JButton button = new JButton(name);
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                commands.get(name).execute();
+            }
+        });
+        return button;
+    }
+    
+
+}
